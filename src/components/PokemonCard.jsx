b941d0525c6ba16c26e8bdd6d95be4654e08592d
ktypik <benjamin.laroche28@gmail.com>
2023-03-29 12:02:13 +0200

    react4

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,4 @@
-function PokemonCard() {
-  const pokemon = pokemonList[0];
+function PokemonCard({ pokemon }) {
   return (
     <figure>
       {pokemon.imgSrc ? (
@@ -12,15 +11,15 @@ function PokemonCard() {
   );
 }
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+//const pokemonList = [
+//  {
+//    name: "bulbasaur",
+//  imgSrc:
+//      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+//  },
+//  {
+//    name: "mew",
+//  },
+//];
 
 export default PokemonCard;
